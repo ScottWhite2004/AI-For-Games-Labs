414e623d35eb3079451408f3244874d19b285554
ScottWhite2004 <115008276+ScottWhite2004@users.noreply.github.com>
2023-10-17 14:36:08 +0100

    I have created a polygon and triangle class that inherits from the shape class

--- AI For Games Lab/AI For Games Lab/Game1.cs
+++ AI For Games Lab/AI For Games Lab/Game1.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.Xna.Framework;
+﻿using AI_For_Games_Lab.Shapes;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGame.ImGui;
+using System.Collections.Generic;
+
 
 namespace AI_For_Games_Lab
 {
@@ -11,6 +14,7 @@ namespace AI_For_Games_Lab
         private SpriteBatch _spriteBatch;
         private ShapeBatcher _shapeBatcher;
         private ImGuiRenderer _guiRenderer;
+        private List<Shape> _shapes;
 
         public Game1()
         {
@@ -24,12 +28,23 @@ namespace AI_For_Games_Lab
             // TODO: Add your initialization logic here
             _shapeBatcher = new ShapeBatcher(this);
             _guiRenderer = new ImGuiRenderer(this).Initialize().RebuildFontAtlas();
+            _shapes = new List<Shape>();
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _shapes.Add(new Circle(new Vector2(100, 100), Color.Red, 30));
+            _shapes.Add(new Circle(new Vector2(200, 150), Color.Red, 20));
+            _shapes.Add(new Circle(new Vector2(100, 150), Color.Red, 25));
+            _shapes.Add(new Shapes.Rectangle(new Vector2(400, 300), Color.Red, 50, 25));
+            _shapes.Add(new Shapes.Rectangle(new Vector2(500, 500), Color.Red, 100, 100));
+            _shapes.Add(new Shapes.Rectangle(new Vector2(100, 200), Color.Red, 100, 25));
+            _shapes.Add(new Triangle(new Vector2(200, 200), Color.Red, new Vector2(300, 300), new Vector2(150, 100)));
+            _shapes.Add(new Polygon(new Vector2(300, 300), Color.Red, new List<Vector2> { new Vector2(400, 350), new Vector2(100, 75), new Vector2(250, 200) }));
+
+
 
             // TODO: use this.Content to load your game content here
         }
@@ -38,6 +53,19 @@ namespace AI_For_Games_Lab
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            Vector2 mousePointInShapeSpace = Mouse.GetState().Position.ToVector2().FlipY(GraphicsDevice.Viewport.Height);
+
+            foreach (Shape shape in _shapes)
+            {
+                if (shape.isInside(mousePointInShapeSpace))
+                {
+                    shape.Colour = Color.Green;
+                }
+                else
+                {
+                    shape.Colour = Color.Red;
+                }
+            }
 
             // TODO: Add your update logic here
 
@@ -47,6 +75,13 @@ namespace AI_For_Games_Lab
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _shapeBatcher.Begin();
+            foreach (Shape shape in _shapes)
+            {
+                _shapeBatcher.Draw(shape);
+            }
+            _shapeBatcher.End();
 
             // TODO: Add your drawing code here
 
